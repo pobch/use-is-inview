2c33d36d1cab6610abb864f4959f2027bee0ec16
Pob <590650@gmail.com>
2020-03-09 21:36:47 +0700

    mock console.log in cypress

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,20 @@
 /// <reference types="cypress" />
 
+let spy
+Cypress.on('window:before:load', win => {
+  spy = cy.spy(win.console, 'log')
+})
+
 it('scroll to target element', () => {
-  cy.visit('http://localhost:3000')
+  cy.visit('http://localhost:3000').should(() => {
+    expect(spy).to.be.calledWith('is intersect:', false)
+  })
+  cy.contains(/target element/i)
+    .scrollIntoView()
+    .should(() => {
+      expect(spy).to.be.calledWith('is intersect:', true)
+    })
+  cy.scrollTo('bottom').should(() => {
+    expect(spy).to.be.calledWith('is intersect:', false)
+  })
 })
